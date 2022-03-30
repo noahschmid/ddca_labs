b3d14c59e3958496a64e5835bb4b291942fd2cd6
noahschmid <noah.schmid77@gmail.com>
2022-03-30 19:40:09 +0200

    Add fsm.v

--- lab4/lab4.srcs/sources_1/new/fsm.v
+++ lab4/lab4.srcs/sources_1/new/fsm.v
@@ -1,4 +1,37 @@
-module fsm(input clk, input rst, input left, input right, output reg[2:0] l_light, output reg[2:0] r_light);
+module fsm(input clk, input reset, input left, input right, output LA, output LB, output LC, output RA, output RB, output RC);
     wire w_clk;
-    clk_div new_clk(clk, rst, w_clk);
+    reg[2:0] state, next_state;
+    wire[5:0] w_lights;
+    
+    parameter S0 = 3'd0; parameter L1 = 3'd1; parameter L2 = 3'd2; parameter L3 = 3'd3;
+    parameter R1 = 3'd4; parameter R2 = 3'd5; parameter R3 = 3'd6;
+    
+    clk_div(clk, rst, w_clk);
+    
+    always @(posedge w_clk, posedge rst)    // state register
+        if(rst) state = S0;
+        else state <= next_state;
+        
+        
+    always @(state)                         // next state logic
+        case(state)
+            S0: 
+            begin
+                if(left) next_state <= L1;
+                else if(right) next_state <= R1;
+                else next_state = S0;
+            end
+            L1: next_state = L2;
+            L2: next_state = L3;
+            R1: next_state = R2;
+            R2: next_state = R3;
+            default: next_state = S0;
+        endcase
+               
+    assign LA = state == L1 || state == L2 || state == L3;     // output logic
+    assign LB = state == L2 || state == L3;
+    assign LC = state == L3;
+    assign RA = state == R1 || state == R2 || state == R3;
+    assign RB = state == R2 || state == R3;
+    assign RC = state == R3;
 endmodule
