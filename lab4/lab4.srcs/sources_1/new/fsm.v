bdd1992313f265a28c0e964eec07d499028afde4
noahschmid <noah.schmid77@gmail.com>
2022-03-30 16:30:13 +0200

    Change fsm module

--- lab4/lab4.srcs/sources_1/new/fsm.v
+++ lab4/lab4.srcs/sources_1/new/fsm.v
@@ -1,4 +1,4 @@
-module fsm(input clk, input rst, output[2:0] left, output[2:0] right);
+module fsm(input clk, input rst, input left, input right, output reg[2:0] l_light, output reg[2:0] r_light);
     wire w_clk;
     clk_div new_clk(clk, rst, w_clk);
 endmodule
