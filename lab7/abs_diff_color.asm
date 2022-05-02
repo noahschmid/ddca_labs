3e9b20d30608ca7f3b08920969ce46d94dee9c1a
noahschmid <noah.schmid77@gmail.com>
2022-05-02 18:29:28 +0200

    Fix mistakes and add comments

--- lab7/abs_diff_color.asm
+++ lab7/abs_diff_color.asm
@@ -11,37 +11,39 @@
 # li $t0, 5 
 # li $t1, 6 
 #
-# addi $sp, $sp, -8
-# sw $t0, 4($sp)
-# sw $t1, 0($sp)
+# # according to MIPS convention, we should always allocate space on the stack for 
+# # the callee to save its arguments on the stack (if it needs to call another function)
+# # and allocate space to save $ra
+# addi $sp, $sp, -28	# make place for 7 values on the stack (6 arguments + return address)
+# sw $t0, 20($sp)		# store the last two arguments on the stack
+# sw $t1, 24($sp)		# the first four are passed via registers
 # jal abs_diff
 
-
 abs_diff:
-	add $t0, $a0, $0	# first store the arguments $a0 to $a3 in $t0..$t3
-	add $t1, $a1, $0
-	add $t2, $a2, $0
-	add $t3, $a3, $0
+	sw $ra, 0($sp)		# store the return address on the stack
+	sw $a0, 4($sp) 		# store the arguments $a0 to $a3 on the stack
+	sw $a1, 8($sp)
+	sw $a2, 12($sp)
+	sw $a3, 16($sp)
 	
-	lw $t4, 4($sp)		# get the last two arguments from the stack
-	lw $t5, 0($sp)
-	
-	addi $sp, $sp, 8	# increment stack pointer as the values are now in registers
-	addi $v1, $0, 0		# result = 0
-	
-	add $a1, $t3, $0	# copy second argument from $t3 (R2) 
+	move $a1, $a3		# copy second argument from $a3 (R2) 
 	jal abs_diff_helper	# call subroutine (first argument is still in a0 - R1)
-	add $v1, $v1, $v0	# store result from abs_diff_helper in v1
+	move $t6, $v0		# store result from abs_diff_helper in t6
 	
-	add $a0, $t1, $0	# copy arguments from $t1 and $t4 to $a0 and $a1
-	add $a1, $t4, $0
+	lw $a0, 8($sp)		# load arguments 2 and 5 from stack
+	lw $a1, 20($sp)
 	jal abs_diff_helper	# call subroutine
-	add $v1, $v1, $v0 	# add result from abs_diff_helper to v1
+	add $t6, $t6, $v0 	# add result from abs_diff_helper to t6
 	
-	add $a0, $t2, $0	# copy arguments from $t2 and $t5 to $a0 and $a1
-	add $a1, $t5, $0
+	lw $a0, 12($sp)		# load arguments 3 and 6 from stack
+	lw $a1, 24($sp)
 	jal abs_diff_helper	# call subroutine
-	add $v0, $v0, $v1	# add result from abs_diff_helper to v1 and store in v0
+	add $t6, $t6, $v0 	# add result from abs_diff_helper to t6
+	
+	move $v0, $t6		# store result in v0
+	lw $ra, 0($sp)		# restore return address
+	addi $sp, $sp, 24	# reset stack pointer as space is no longer needed
+	jr $ra			# exit function
 
 abs_diff_helper:		# we use abs_diff_helper as a subroutine that behaves just like the original abs_diff
 	sub $t1, $a0, $a1
